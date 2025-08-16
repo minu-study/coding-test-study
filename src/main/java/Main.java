1eea0e9fb37b9ad1a53a4f8943f9f6a9a0a410da
leo <leo.jeong@h2ohospitality.io>
2025-08-16 20:54:59 +0900

    feat: 섹션1 - 문장 속 가장 긴 단어 찾기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 package main.java;
 
 import main.java.main.java.solutions.section1.ToggleCase;
+import main.java.main.java.solutions.section1.WordsInSentence;
 
 public class Main {
 
     public static void main(String[] args){
         java.util.Scanner in = new java.util.Scanner(System.in);
         String line = in.nextLine();
-        String result = ToggleCase.setCharacter(new String[]{line});
+        String result = WordsInSentence.getLongString(line);
         System.out.println(result);
     }
 
--- src/main/java/main/java/solutions/section1/WordsInSentence.java
+++ src/main/java/main/java/solutions/section1/WordsInSentence.java
@@ -5,4 +5,18 @@ package main.java.main.java.solutions.section1;
  * Placeholder for study; no implementation.
  */
 public class WordsInSentence {
+
+    public static String getLongString(String input) {
+
+        String[] Strings = input.split(" ");
+        String result = "";
+
+        for (String string : Strings) {
+            if (string.length() > result.length()){
+                result = string;
+            }
+        }
+        return result.trim();
+    }
+
 }

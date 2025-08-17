46773d4d42ba67180718e448cd532608bd2fa9b4
leo <leo.jeong@h2ohospitality.io>
2025-08-17 17:59:42 +0900

    feat: 섹션1 - 단어 뒤집기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
 package main.java;
 
-import main.java.main.java.solutions.section1.ToggleCase;
+import main.java.main.java.solutions.section1.ReverseWords;
 import main.java.main.java.solutions.section1.WordsInSentence;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        java.util.Scanner in = new java.util.Scanner(System.in);
-        String line = in.nextLine();
-        String result = WordsInSentence.getLongString(line);
-        System.out.println(result);
+        Scanner in = new Scanner(System.in);
+        List<String> resultList = ReverseWords.reverseWords(in);
+        for (String result : resultList) {
+            System.out.println(result);
+        }
     }
 
 }
--- src/main/java/main/java/solutions/section1/ReverseWords.java
+++ src/main/java/main/java/solutions/section1/ReverseWords.java
@@ -1,8 +1,29 @@
 package main.java.main.java.solutions.section1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Section 1 - String: 4. Reverse Words
  * Placeholder for study; no implementation.
  */
 public class ReverseWords {
+
+    public static List<String> reverseWords(Scanner in) {
+
+        List<String> result = new ArrayList<>();
+        int n = in.nextInt();
+        String[] inputArray = new String[n];
+        for(String input : inputArray) {
+            input = in.next();
+            result.add(reverse(input));
+        }
+        return result;
+    }
+
+    private static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
 }
